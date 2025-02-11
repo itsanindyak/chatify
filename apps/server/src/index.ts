4538063fbe0583ee5c93bd46f6d1ecb47f0cf303
itsanindyak <iankoley04@gmail.com>
2025-02-11 23:16:10 +0530

    create initial node server

---
+++ apps/server/src/index.ts
@@ -0,0 +1,14 @@
+import http from 'http'
+
+
+async function init() {
+   
+    const server = http.createServer();
+    const PORT = process.env.PORT ?process.env.PORT : 8000
+
+    server.listen(PORT,()=>console.log(`server is running http://localhost:${PORT}`));
+
+    
+}
+
+init()
