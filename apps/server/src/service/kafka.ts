11fc03aa2c74e184350aa07b11d00c14fb69f195
itsanindyak <iankoley04@gmail.com>
2025-02-20 19:35:34 +0530

    create message producer for kafka

--- apps/server/src/service/kafka.ts
+++ apps/server/src/service/kafka.ts
@@ -1,18 +1,37 @@
-import { Kafka } from "kafkajs";
+import { Kafka, Producer } from "kafkajs";
+import fs from "fs"
+import path from "path";
 
 const kafka = new Kafka({
-    brokers:[]
+    brokers:[process.env.KAFKA_URL as string],
+    ssl:{
+        ca:[fs.readFileSync(path.resolve("ca.pem"),"utf-8")]
+    },
+    sasl:{
+        username: process.env.KAFKA_USERNAME as string,
+        password: process.env.KAFKA_PASSWORD as string,
+        mechanism:"plain"
+    }
 })
 
-
+let producer : Producer | null = null
 const createProducer = async ()=>{
-    const producer = kafka.producer()
-    await producer.connect()
+    if(producer) return producer
+
+    const _producer = kafka.producer()
+    await _producer.connect()
+    producer = _producer
     return  producer
 }
 
-const produceMessage = (key:string,message:string)=>{
-    
+const produceMessage = async (message:string)=>{
+    const producer= await createProducer()
+    await producer.send({
+        topic:"MESSAGE",
+        messages:[{key: `msg-${Date.now()}`,value: message}]
+    })
+
+    return true
 }
 
-export default kafka
+export  {kafka,produceMessage}
