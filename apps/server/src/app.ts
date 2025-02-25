b99918891118cd0c2d78494fa41f7b3b23a6e217
itsanindyak <iankoley04@gmail.com>
2025-02-25 23:18:49 +0530

    initialize Express app and integrate cookie-parser middleware

---
+++ apps/server/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import cookieParser from "cookie-parser";
+
+
+
+const app = express()
+
+app.use(cookieParser())
+
+
+
+
+export {app}
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,11 +2,12 @@ import 'dotenv/config'
 import http from 'http'
 import socketServices from './service/socket';
 import { startMessageConsume } from './service/kafka';
+import { app } from './app';
 
 async function init() {
     startMessageConsume()
     const socket = new socketServices()
-    const server = http.createServer();
+    const server = http.createServer(app);
     const PORT = process.env.PORT ?process.env.PORT : 8000
 
     socket.io.attach(server)
