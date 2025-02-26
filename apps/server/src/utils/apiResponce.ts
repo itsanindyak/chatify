454faec5265bd583714dca520b475f6fa29cac5e
itsanindyak <iankoley04@gmail.com>
2025-02-26 20:44:45 +0530

    feat: implement ApiResponce class for standardized API responses

---
+++ apps/server/src/utils/apiResponce.ts
@@ -0,0 +1,26 @@
+type HttpStatusCode= 200 |201 ;
+
+const statusCodeMap:Record<HttpStatusCode,string> = {
+    200: 'OK',
+    201: 'Created',
+}
+
+
+
+class ApiResponce{
+    public statusCode
+    public data
+    public code
+    public message
+    constructor(
+        statusCode: HttpStatusCode,
+        data : any,
+        message?: string,
+
+    ){
+        this.statusCode = statusCode
+        this.code = statusCodeMap[statusCode]
+        this.data= data
+        this.message= message
+    }
+}
