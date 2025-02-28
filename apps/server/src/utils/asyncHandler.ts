d4e80c63eebfd928ef1b609bb797565e12c03d64
itsanindyak <iankoley04@gmail.com>
2025-02-28 19:45:58 +0530

    refactor: improve asyncHandler and update generateToken function to handle environment variables

--- apps/server/src/utils/asyncHandler.ts
+++ apps/server/src/utils/asyncHandler.ts
@@ -1,11 +1,9 @@
-import { Request,Response,NextFunction, Handler } from "express";
+import { Request, Response, NextFunction, Handler } from "express";
 
-
-
-export const asyncHandler = (requestHandler: Handler) => {
-    return (req:Request, res :Response, next:NextFunction) => {
-      Promise.resolve(requestHandler(req, res, next)).catch((error) =>
-        next(error)
-      );
-    };
+export const asyncHandler = (requestHandler: any) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
   };
+};
--- apps/server/src/utils/token.ts
+++ apps/server/src/utils/token.ts
@@ -1,8 +1,18 @@
 import jwt from "jsonwebtoken";
+import { StringValue } from "ms";
 
-const generateToken = async (data: string) => {
-  const token = await jwt.sign(data, process.env.TOKEN_SECERT as string, {
-    expiresIn: parseInt(process.env.TOKEN_EXPIRE as string, 10),
+const generateToken = (data: object): string => {
+  const secret = process.env.TOKEN_SECERT;
+  const expiresIn = process.env.TOKEN_EXPIRE || "1h";
+
+  console.log(secret, " ", expiresIn);
+
+  if (!secret) {
+    throw new Error("TOKEN_SECRET is not defined in environment variables");
+  }
+
+  const token = jwt.sign(data, secret, {
+    expiresIn: process.env.TOKEN_EXPIRE as StringValue,
   });
 
   return token;
