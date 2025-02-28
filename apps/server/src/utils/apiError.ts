d433b415b831ecc7cf7e71caa47ce0e2fe5fd2d3
itsanindyak <iankoley04@gmail.com>
2025-02-28 19:45:12 +0530

    refactor: update ApiError class to include 409 status code and improve formatting

--- apps/server/src/utils/apiError.ts
+++ apps/server/src/utils/apiError.ts
@@ -1,30 +1,23 @@
-type ErrorStatusCode= 400 | 401 | 403 | 404 | 500 | 503;
+type ErrorStatusCode = 400 | 401 | 403 | 404 | 409 | 500 | 503;
 
-const ErrorstatusCodeMap:Record<ErrorStatusCode,string> = {
-    400: "Bad Request",
-    401: "Unauthorized",
-    403: "Forbidden",   
-    404: "Not Found",
-    500: "Internal Server Error",
-    503: "Service Unavailable"
-}
+const ErrorstatusCodeMap: Record<ErrorStatusCode, string> = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "",
+  500: "Internal Server Error",
+  503: "Service Unavailable",
+};
 
-
-
-export class ApiError extends Error{
-    public statusCode
-    public data
-    public code
-    constructor(
-        message: string,
-        statusCode: ErrorStatusCode,
-        data? : string
-
-    ){
-        super(message)
-        this.statusCode = statusCode
-        this.code = ErrorstatusCodeMap[statusCode]
-        this.data= data
-
-    }
+export class ApiError extends Error {
+  public statusCode;
+  public data;
+  public code;
+  constructor(message: string, statusCode: ErrorStatusCode, data?: string) {
+    super(message);
+    this.statusCode = statusCode;
+    this.code = ErrorstatusCodeMap[statusCode];
+    this.data = data;
+  }
 }
