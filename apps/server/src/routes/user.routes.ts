7817ce0711230268cee88d5b6ed9bcb9e58ce8db
itsanindyak <iankoley04@gmail.com>
2025-03-03 23:59:07 +0530

    feat: add logout route to user routes with authentication middleware

--- apps/server/src/routes/user.routes.ts
+++ apps/server/src/routes/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import userControllers from "../controllers/user.controllers";
+import auth from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.post("/signup", userControllers.signUp);
 router.post("/login", userControllers.login);
+
+router.route("/logout").post(auth,userControllers.logout)
 
 export default router;
